0fb2b709e88b4eba84841f89a56f7c5b41014f4f
Megan Gonzales <megan.campbell26@yahoo.com>
2021-07-31 12:56:43 -0600

    Install yarn, webpacker set up faker to seed db

---
+++ db/migrate/20210731184441_change_doc_type.rb
@@ -0,0 +1,5 @@
+class ChangeDocType < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :documents, :type, :doc_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_174439) do
+ActiveRecord::Schema.define(version: 2021_07_31_184441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_07_31_174439) do
   end
 
   create_table "documents", force: :cascade do |t|
-    t.string "type"
+    t.string "doc_type"
     t.date "expiration"
     t.bigint "clinician_id", null: false
     t.datetime "created_at", precision: 6, null: false
